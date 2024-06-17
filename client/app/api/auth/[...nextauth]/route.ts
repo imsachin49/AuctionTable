63741d1f0d7936792536569edadf2b8364e49473
Sachin <kumaraditya7125@gmail.com>
2024-06-17 15:58:41 +0530

    build issue fixed

--- client/app/api/auth/[...nextauth]/route.ts
+++ client/app/api/auth/[...nextauth]/route.ts
@@ -3,40 +3,6 @@ import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import axios from "axios";
 
-// Define a custom User type to include the token property
-interface CustomUser {
-  token?: string; // Assuming token is a string
-  id: string;
-  email: string;
-  name: string;
-  image: string;
-  provider: string;
-  providerAccountId: string;
-  providerId: string;
-  avatar: string;
-}
-
-// Define a custom Account type to include the providerAccountId property
-interface CustomAccount {
-  providerAccountId: string;
-  provider: string;
-  providerId: string;
-}
-
-// Define a custom Profile type to include the image property
-interface CustomProfile {
-  image: string;
-}
-
-// Define a custom Session type to include the user property
-interface CustomSession {
-  user: {
-    id: string;
-    email: string;
-    token: string;
-  };
-}
-
 const handler = NextAuth({
   providers: [
     GithubProvider({
@@ -49,16 +15,19 @@ const handler = NextAuth({
     }),
   ],
   callbacks: {
-    async signIn({ user, account }: { user: CustomUser; account: any }) {
+    async signIn({ user, account }: any) {
       try {
         if (user && account) {
-          const res = await axios.post("https://auctiontable.onrender.com/api/auth/oauth", {
-            email: user.email,
-            username: user.name,
-            provider: account.provider,
-            providerId: account.providerAccountId,
-            avatar: user.image,
-          });
+          const res = await axios.post(
+            "https://auctiontable.onrender.com/api/auth/oauth",
+            {
+              email: user.email,
+              username: user.name,
+              provider: account.provider,
+              providerId: account.providerAccountId,
+              avatar: user.image,
+            }
+          );
           if (res?.data?.data?.token) {
             user.token = res.data.data.token;
           }
@@ -72,7 +41,7 @@ const handler = NextAuth({
         return false;
       }
     },
-    async jwt({ token, user }: { token: any; user: CustomUser }) {
+    async jwt({ token, user }: any) {
       if (user) {
         token.id = user.id;
         token.email = user.email;
@@ -80,17 +49,7 @@ const handler = NextAuth({
       }
       return token;
     },
-    async session({
-      session,
-      user,
-      token,
-    }: {
-      session: CustomSession;
-      user: CustomUser;
-      token: any;
-    }) {
-      console.log("usetr", user);
-      console.log("token", token);
+    async session({ session, user, token }: any) {
       session.user.id = token.id;
       session.user.email = token.email;
       session.user.token = token.token;
