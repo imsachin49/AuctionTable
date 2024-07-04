b5dcb44e8efef4be19eac7e9414851d6d6add869
Sachin <kumaraditya7125@gmail.com>
2024-07-04 09:13:16 +0530

    asynchandler11

--- server/utils/asyncHandler.js
+++ server/utils/asyncHandler.js
@@ -1,7 +1,20 @@
+// const asyncHandler = (requestHandler) => {
+//     return (req, res, next) => {
+//         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+//     }
+// }
+
+// module.exports={asyncHandler};
+
 const asyncHandler = (requestHandler) => {
     return (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
-    }
-}
+        console.log('Request received:', req.method, req.url);
+        console.log('Next function:', next);
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => {
+            console.error('Error caught in asyncHandler:', err);
+            next(err);
+        });
+    };
+};
 
-module.exports={asyncHandler};
+module.exports = { asyncHandler };
